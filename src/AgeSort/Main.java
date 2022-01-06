3e8b2040d270a3a3d97763914e24decaebd7b1d3
shinyubin989 <dbqls2821@gmail.com>
2022-01-06 10:11:57 +0900

    220106-1

---
+++ src/AgeSort/Main.java
@@ -0,0 +1,24 @@
+package AgeSort;
+
+class Pair{
+    int age;
+    String name;
+    public Pair(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+    public int first() {
+        return age;
+    }
+    public String second() {
+        return name;
+    }
+    @Override
+    public String toString() {
+        return age + " " + name;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {}
+}
