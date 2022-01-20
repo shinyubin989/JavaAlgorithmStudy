2a3ef8d25ada98574a54657ab77a16603405a49e
shinyubin989 <dbqls2821@gmail.com>
2022-01-20 09:57:15 +0900

    220120 baekjoon 10828 Stack solved

---
+++ src/Stack/Main.java
@@ -0,0 +1,49 @@
+package Stack;
+
+import java.io.*;
+import java.util.Stack;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    System.setIn(new FileInputStream("src/Stack/input.txt"));
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    int num = Integer.parseInt(br.readLine());
+    String[] input = new String[num];
+    Stack<String> stack = new Stack<>();
+    StringBuilder stringBuilder = new StringBuilder();
+    for (int i = 0; i < num; i++) {
+      input[i] = br.readLine();
+    }
+    for (int i = 0; i < num; i++) {
+      if (input[i].contains(" ")) {
+        String[] newStr = input[i].split(" ");
+        stack.push(newStr[1]);
+      } else if (input[i].equals("pop")) {
+        if (stack.isEmpty()) {
+          stringBuilder.append("-1").append("\n");
+        } else {
+          stringBuilder.append(stack.peek()).append("\n");
+          stack.pop();
+        }
+      } else if (input[i].equals("size")) {
+        stringBuilder.append(stack.size()).append("\n");
+      } else if (input[i].equals("top")) {
+        if (stack.isEmpty()) {
+          stringBuilder.append("-1").append("\n");
+        } else {
+          stringBuilder.append(stack.peek()).append("\n");
+        }
+      } else {
+        //empty
+        if (stack.isEmpty()) {
+          stringBuilder.append("1").append("\n");
+        } else {
+          stringBuilder.append("0").append("\n");
+        }
+      }
+    }
+
+    System.out.println(stringBuilder);
+  }
+}
