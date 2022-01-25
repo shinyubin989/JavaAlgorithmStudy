09770431a61433512a9d5c8d1b7b64de3bf3f291
Yubin <69676101+shinyubin989@users.noreply.github.com>
2022-01-25 09:44:09 +0900

    Update 10866 덱.java

--- src/10866 덱.java
+++ src/10866 덱.java
@@ -9,7 +9,7 @@ import java.util.StringTokenizer;
 
 public class Main {
   public static void main(String[] args) throws IOException {
-    System.setIn(new FileInputStream("src/input.txt"));
+    //System.setIn(new FileInputStream("src/input.txt"));
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringBuilder stringBuilder = new StringBuilder();
     StringTokenizer stringTokenizer;
@@ -66,92 +66,3 @@ public class Main {
 
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//import java.io.*;
-//import java.util.LinkedList;
-//import java.util.PriorityQueue;
-//import java.util.Queue;
-//import java.util.StringTokenizer;
-//
-//class Pair implements Comparable<Pair>{
-//  int docNum;
-//  int priority;
-//
-//  public Pair(int docNum, int priority) {
-//    this.docNum = docNum;
-//    this.priority = priority;
-//  }
-//
-//  @Override
-//  public int compareTo(Pair o) {
-//    return o.priority - priority;
-//  }
-//}
-//
-//public class Main {
-//  public static void main(String[] args) throws IOException {
-//    System.setIn(new FileInputStream("src/input.txt"));
-//    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//    StringBuilder stringBuilder = new StringBuilder();
-//    StringTokenizer stringTokenizer;
-//    Queue<Pair> queue = new LinkedList<>();
-//
-//    int num = Integer.parseInt(br.readLine());
-//
-//    for (int i = 0; i < num; i++) {
-//      stringTokenizer = new StringTokenizer(br.readLine());
-//      int documentNumber = Integer.parseInt(stringTokenizer.nextToken());
-//      int documentPriority = Integer.parseInt(stringTokenizer.nextToken());
-//
-//      stringTokenizer = new StringTokenizer(br.readLine());
-//
-//      stringBuilder.append(count).append("\n");
-//
-//    }
-//    System.out.println(stringBuilder);
-//
-//
-//
-//    //queue.add(1);     // queue에 값 1 추가
-////    queue.poll();       // queue에 첫번째 값을 반환하고 제거 비어있다면 null
-////    queue.remove();     // queue에 첫번째 값 제거
-////    queue.clear();      // queue 초기화
-////    queue.peek();       // queue의 첫번째 값 참조
-//
-//
-//    /**
-//     * 3
-//     *
-//     * 1 0
-//     * 5
-//     * 4 2
-//     * 1 2 3 4
-//     * 6 0
-//     * 1 1 9 1 1 1
-//     */
-//    // 문서의 개수 N(1 ≤ N ≤ 100), 몇 번째로 인쇄되었는지 궁금한 문서가 현재 Queue에서 몇 번째에 놓여 있는지를 나타내는 정수 M(0 ≤ M < N)
-//    // N개 문서의 중요도가 차례대로 주어진다
-//  }
-//}
