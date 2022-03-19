cd76a9106a9119de4ffa2b1c2726ec97b8978a0b
Yubin <69676101+shinyubin989@users.noreply.github.com>
2022-03-20 01:59:48 +0900

    Create 9012_괄호.java

---
+++ src/9012_괄호.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringBuilder sb = new StringBuilder();
+    int num = Integer.parseInt(br.readLine());
+    int count = 0;
+
+    // (()())((()))
+
+    for (int i = 0; i < num; i++) {
+      char[] arr = br.readLine().toCharArray();
+      for (int j = 0; j < arr.length; j++) {
+        if (count >= 0) {
+          if (arr[j] == '(') {
+            count++;
+          } else {
+            count--;
+          }
+        }
+        else {
+          break;
+        }
+      }
+      if (count == 0) {
+        sb.append("YES").append("\n");
+
+      } else {
+        sb.append("NO").append("\n");
+      }
+      count = 0;
+
+    }
+    System.out.println(sb);
+
+  }
+}
+
+/**
+ * 6
+ * (())())
+ * (((()())()
+ * (()())((()))
+ * ((()()(()))(((())))()
+ * ()()()()(()()())()
+ * (()((())()(
+ */
